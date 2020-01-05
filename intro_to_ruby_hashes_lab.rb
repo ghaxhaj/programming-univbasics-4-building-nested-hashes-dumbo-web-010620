fc515dc895e2e080c04c40acc7b8bb0b08c2db56
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-05 22:29:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,12 @@ def monopoly_with_third_tier
         :key3 => {},
         :key4 => {}
       },
-      :names => {}
+      :names => {
+        :key => {},
+        :key2 => {},
+        :key3 => {},
+        key4 => {}
+      }
     }
 }
 end
